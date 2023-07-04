45400e242d49c048e474099e6320cb5bdae7751a
Walter <walterdcarrizo.19@gmail.com>
2023-07-04 15:56:58 -0300

    Se creó el componente card y se lo colocó en la pag principal

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,25 @@
 import React, { useEffect } from 'react'
-import { Container, Button, Stack } from '@mui/material';
-import { HeaderComponent } from '../components';
-import { result } from '../api/base.api';
+import { Container, Button, Stack, Grid, Typography, Box } from '@mui/material';
+import { CardComponent, HeaderComponent } from '../components';
+import { useState } from 'react';
+import { TypeProducts } from '../types/products.interface';
+
 
 export const HomePage: React.FC<{}> = () => {
 
+  const [products, setProducts] = useState<TypeProducts[] | null >(null)
   useEffect(() => {
-    result
+       fetch('https://fakestoreapi.com/products')
+                    .then(res=>res.json())
+                    .then(json=>setProducts(json))
+                    .catch(Error)
   }, [])
   
   return (
-      <Container sx={{ mt: 9 }} maxWidth="xl">
-        <Container maxWidth="xl">
+      <Container sx={{ mt: 3 }} maxWidth="xl">
           <HeaderComponent 
             title='Bienvenidos a Purple Store'
-            description='Aquí podrás encontrar lo último en moda para la temporada otoño/invierno 2023'
+            description='Aquí podrás encontrar lo último en moda, joyería y tecnología.'
             element={
               <Stack spacing={2} direction='row'>
                 <Button variant='contained'>Hola mundo</Button>
@@ -22,7 +27,27 @@ export const HomePage: React.FC<{}> = () => {
               </Stack>
             }
           />
-        </Container>
+          <Box>
+            {
+              (!products) ? 
+                          <Typography variant='h5'>No se encontraron productos</Typography>
+                          :
+                          <Grid container sx={{ mt:3 }}>
+                            {
+                              products.map(product => (
+                                <Grid item xs={3} key={product.id}>
+                                  <CardComponent  
+                                      title={product.title} 
+                                      description={product.description}
+                                      price={product.price}
+                                      img={product.image}
+                                  />
+                                </Grid>
+                              ))
+                            }
+                          </Grid>
+            }
+          </Box>
       </Container>
   )
 }
