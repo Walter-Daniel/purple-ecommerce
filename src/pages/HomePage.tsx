c8fa2abd2f6959fd629c9dfa2a3e64756310f3e3
Walter <walterdcarrizo.19@gmail.com>
2023-07-06 19:27:44 -0300

    Sección check y filtro de productos

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { useFetch } from '../api/base.api';
 import { Container, Button, Stack, Grid, Typography, CircularProgress} from '@mui/material';
 import { CardComponent, HeaderComponent } from '../components';
+import { Category } from '../components/CheckCategory';
 
 export const HomePage: React.FC<{}> = () => {
 
   const { productsFilter, loading, error } = useFetch('https://fakestoreapi.com/products')
   
   return (
-      <Container sx={{ mt: 3 }} maxWidth="xl">
+      <Container sx={{ mt: 15 }} maxWidth="xl">
           <HeaderComponent 
             title='Bienvenidos a Purple Store'
             description='Aquí podrás encontrar lo último en moda, joyería y tecnología.'
@@ -19,7 +20,8 @@ export const HomePage: React.FC<{}> = () => {
               </Stack>
             }
           />
-          <Grid container sx={{ mt:3 }}>
+          <Category products={productsFilter}/>
+          <Grid container sx={{ mt:3, width:'100%' }} justifyContent='center'>
             {error && 
               <Grid item textAlign='center'>
                 <Typography variant='h3'>Se ha producido un error al cargar los productos</Typography>
@@ -27,13 +29,15 @@ export const HomePage: React.FC<{}> = () => {
             }
             {
               loading &&
-              <Grid item justifyContent='center' sx={{ width: '100%' }}>
-                <CircularProgress color="secondary" />
+              <Grid container justifyContent='center' direction='row'>
+                <Grid item xs={1}>
+                  <CircularProgress color="secondary"/>
+                </Grid>
               </Grid>
             }
             {
               productsFilter?.map(product => (               
-                <Grid item xs={3} key={product.id}>
+                <Grid item xs={4} key={product.id} sx={{ mt: 4, width:'100%', display: 'flex', justifyContent:'center'}} >
                   <CardComponent  
                       title={product.title} 
                       description={product.description}
