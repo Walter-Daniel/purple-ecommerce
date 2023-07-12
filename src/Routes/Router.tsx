15b4adc1298225ae0b59cb6d3ea5484c8ab30c90
Walter <walterdcarrizo.19@gmail.com>
2023-07-12 15:24:15 -0300

    Se crea cartPage, se modifica la rutas de inicio y cartPage

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { HomePage, LoginPage, RegisterPage } from '../pages';
+import { CartPage, HomePage, LoginPage, RegisterPage } from '../pages';
 import { RouterLayout } from '../common/RouterLayout';
 
 export const AppRouter: React.FC<{}> = () => {
@@ -8,6 +8,7 @@ export const AppRouter: React.FC<{}> = () => {
         <Routes>
             <Route path='/' element={<RouterLayout/>}>
                 <Route path='/' element={<HomePage/>} />           
+                <Route path='/cart' element={<CartPage />} />           
             </Route>
             <Route path='/login' element={<LoginPage/>} />
             <Route path='/register' element={<RegisterPage />} /> 
