01146c45e28117a47677730690bc9322bc1ae43d
Walter <walterdcarrizo.19@gmail.com>
2023-07-21 18:05:05 -0300

    Se conecto el formulario de resgistro a firebase, se pueden crear usuarios

--- src/firebase/provider.ts
+++ src/firebase/provider.ts
@@ -1,5 +1,7 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
 import { FirebaseApp } from "./config";
+import { useAppSelector } from "../redux";
+import { RegisterProps } from "../pages";
 
 const provider = new GoogleAuthProvider();
 const auth = getAuth(FirebaseApp);
@@ -14,4 +16,21 @@ export const signInWithGoogle = async() => {
         console.log(error);
         
     }   
+}
+
+export const registerWithEmail= async({ firstName, lastName, email, password}:RegisterProps) => {
+    try {
+        const resp = await createUserWithEmailAndPassword(auth, email, password);
+        const { uid} = resp.user;
+        console.log(resp);
+        console.log({uid});
+        
+    } catch (error) {
+        console.log(error);
+        
+        // return {
+        //     ok: false,
+        //     errorMessage
+        // }
+    }
 }
--- src/redux/auth/thunk.ts
+++ src/redux/auth/thunk.ts
@@ -1,10 +1,20 @@
 import { AppDispatch, checkingCredentials } from ".."
-import { signInWithGoogle } from "../../firebase/provider"
+import { signInWithGoogle, registerWithEmail } from "../../firebase/provider"
+import { RegisterProps } from "../../pages"
 
 export const startGoogleSignIn = () => {
     return async(dispatch : AppDispatch) => {
         dispatch( checkingCredentials() )
         const result = signInWithGoogle();
-        
+        console.log(result)
+    }
+}
+
+export const startRegisterWithEmail = ({ firstName, lastName, email, password }:RegisterProps) => {
+    return async(dispatch:AppDispatch) => {
+        dispatch( checkingCredentials() );
+        const resp = await registerWithEmail({ firstName, lastName, email, password });
+        console.log(resp)
+
     }
 }
