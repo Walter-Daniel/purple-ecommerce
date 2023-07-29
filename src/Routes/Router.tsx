c3e93131d79ba495132ffc4573e7d336ac365f3e
Walter <walterdcarrizo.19@gmail.com>
2023-07-29 20:25:44 -0300

    Se creó ErrorPage, y se escribieron condiciones para el ingreso de las rutas

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,47 +1,13 @@
-
 import { createBrowserRouter } from 'react-router-dom';
-
 import { AdminRoutes, AuthRoutes } from './Conditions.routes';
 import { RouterLayout } from '../common/RouterLayout';
-import { HomePage, LoginPage, ProductsPage, RegisterPage } from '../pages';
-
-
-// export const AppRouter: React.FC<{}> = () => {    
-//     const { status } = useAppSelector((state) => state.authReducer)
-
-//     return(
-        // <Routes>
-        //     <Route path='/' element={<RouterLayout/>}>
-        //         <Route path='/' element={<HomePage/>} /> 
-        //         <Route path='/*' element={ <Navigate to="/" /> } /> 
-        //         <Route path='/user' element={<PrivateRoute />} >
-        //             <Route path='/user/cart' element={<CartPage />} />
-        //             <Route path="/user/*" element={ <Navigate to="/login" /> } />
-        //         </Route>
-        //     </Route>
-        //     <Route path='/login' element={<LoginPage/>} />
-        //     <Route path='/register' element={<RegisterPage />} />
-        //     <Route path='/admin' element={ < AdministrationPage /> } />
-        // </Routes>
-        // <Routes>
-        //     <Route path='/' element={<RouterLayout />}>
-        //         <Route path='/' element={<HomePage />} /> 
-        //         <Route path='/product/:ID' element={<HomePage/>} /> 
-        //         <Route path='/*' element={ <Navigate to="/" /> } /> 
-        //     </Route>
-        //     <Route path='/auth' element={<AuthRoutes />}>
-        //         <Route path='/auth/login' element={<LoginPage />} />
-        //         <Route path='/auth/register' element={<RegisterPage />} />
-        //         <Route path='/auth/*' element={ <Navigate to="/" /> } /> 
-        //     </Route>
-        // </Routes>
-//     )
-// }
+import { CartPage, ErrorPage, HomePage, LoginPage, ProductsPage, RegisterPage } from '../pages';
 
 export const router= createBrowserRouter([
     {
         path:'/',
         element: <RouterLayout />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 index: true,
@@ -52,20 +18,6 @@ export const router= createBrowserRouter([
                 element: <HomePage /> //CREAR
             },
             {
-                path: 'auth',
-                element: <AuthRoutes />,
-                children:[
-                    {
-                        path: 'login',
-                        element: <LoginPage />
-                    },
-                    {
-                        path: 'register',
-                        element: <RegisterPage />
-                    }
-                ]
-            },
-            {
                 path: 'admin',
                 element: <AdminRoutes />,
                 children: [
@@ -74,7 +26,26 @@ export const router= createBrowserRouter([
                         element: <ProductsPage />
                     }
                 ]
+            },
+            {
+                path: 'user',
+                element: <CartPage />
+            }
+        ],
+        
+    },
+    {
+        path: '/auth',
+        element: <AuthRoutes />,
+        children:[
+            {
+                path: 'login',
+                element: <LoginPage />
+            },
+            {
+                path: 'register',
+                element: <RegisterPage />
             }
         ]
-    }
+    },
 ])
